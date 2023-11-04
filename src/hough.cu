fd4cbf1e827f6751260e342798324f6b770dcd74
Santiago Taracena <tar20017@uvg.edu.gt>
2023-11-04 10:27:09 -0600

    Created actual hough.cu file

---
+++ src/hough.cu
@@ -0,0 +1,207 @@
+/**
+ * Universidad del Valle de Guatemala
+ * (CC3069) Computación Paralela y Distribuida
+ * Proyecto 03 - Transformada de Hough con CUDA
+ * 
+ * Miembros del equipo de trabajo:
+ * - José Rodrigo Barrera García (20807)
+ * - Marco Pablo Orozco Saravia (20857)
+ * - Santiago Taracena Puga (20017)
+ */
+
+// Librerías necesarias para el desarrollo del proyecto.
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+#include <string.h>
+#include "misc/pgm.h"
+
+// Constantes globales importantes para la transformada de Hough.
+const int degreeInc = 2;
+const int degreeBins = (180 / degreeInc);
+const int rBins = 100;
+const float radInc = ((degreeInc * M_PI) / 180);
+
+// Función CPU_HoughTran, que calcula la transformada de Hough de forma secuencial.
+void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc) {
+
+  // Cálculo del r máximo a utilizar y asignación de memoria.
+  float rMax = sqrt(1.0 * w * w + 1.0 * h * h) / 2;
+  *acc = new int[rBins * degreeBins];
+  memset(*acc, 0, (sizeof(int) * rBins * degreeBins));
+
+  // Cálculo del centro de la imagen para utilizar como origen.
+  int xCent = (w / 2);
+  int yCent = (h / 2);
+  float rScale = ((2 * rMax) / rBins);
+
+  // Iteración sobre los pixeles de la imagen.
+  for (int i = 0; i < w; i++) {
+    for (int j = 0; j < h; j++) {
+
+      // Cálculo del índice a utilizar en la iteración.
+      int idx = ((j * w) + i);
+
+      // Verificación de que el pixel de la imagen no sea negro para calcular la transformada.
+      if (pic[idx] > 0) {
+
+        // Coordenada en x y en y.
+        int xCoord = (i - xCent);
+        int yCoord = (yCent - j);
+
+        // Theta inicial a utilizar como prueba.
+        float theta = 0;
+
+        // Iteración sobre el rango de ángulos a utilizar.
+        for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+
+          // Cálculo del valor de r de la iteración.
+          float r = ((xCoord * cos(theta)) + (yCoord * sin(theta)));
+
+          // Cálculo del índice a probar.
+          int rIdx = ((r + rMax) / rScale);
+
+          // Aumento del índice que se utilizó.
+          (*acc)[rIdx * degreeBins + tIdx]++;
+
+          // Aumento de theta según el incremento configurado.
+          theta += radInc;
+        }
+      }
+    }
+  }
+}
+
+// Kernel del programa utilizado para paralelizar el proceso de cálculo de la transformada.
+__global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float *d_Cos, float *d_Sin) {
+
+  // Cálculo y verificación de que el ID sea válido.
+  int gloID = threadIdx.x + blockIdx.x * blockDim.x;
+  if (gloID > (w * h)) return;
+
+  // Cálculo del centro de la imagen.
+  int xCent = (w / 2);
+  int yCent = (h / 2);
+
+  // Coordenada o pixel a utilizar en el presente kernel.
+  int xCoord = ((gloID % w) - xCent);
+  int yCoord = (yCent - (gloID / w));
+
+  // Verificación de que el pixel iterado no sea negro.
+  if (pic[gloID] > 0) {
+
+    // Ciclo for que itera en los ángulos a probar por el kernel.
+    for (int tIdx = 0; tIdx < degreeBins; tIdx++) {
+
+      // Cálculo del r a probar y adición del resultado.
+      float r = ((xCoord * d_Cos[tIdx]) + (yCoord * d_Sin[tIdx]));
+      int rIdx = ((r + rMax) / rScale);
+      atomicAdd(acc + (rIdx * degreeBins + tIdx), 1);
+    }
+  }
+}
+
+// Función main que se encarga de ejecutar el programa.
+int main (int argc, char **argv) {
+
+  // Verificación de errores en caso de no pasar una imagen.
+  if (argc < 2) {
+    printf("Usage: ./hough <pgm-image>\n");
+    return EXIT_FAILURE;
+  }
+
+  // Carga de la imagen pasada como argumento de consola.
+  PGMImage inImg(argv[1]);
+
+  // Obtención del ancho y alto de la imagen.
+  int *cpuht;
+  int w = inImg.x_dim;
+  int h = inImg.y_dim;
+
+  // Instancia de d_Cos y d_Sin a utiliizar.
+  float* d_Cos;
+  float* d_Sin;
+
+  // Alocación de memoria en la GPU.
+  cudaMalloc((void**) &d_Cos, sizeof(float)* degreeBins);
+  cudaMalloc((void**) &d_Sin, sizeof(float)* degreeBins);
+
+  // Llamada a la versión secuencial del cálculo de la transformada de Hough.
+  CPU_HoughTran(inImg.pixels, w, h, &cpuht);
+
+  // Obtención de pcCos, pcSin y los radiantes iniciales.
+  float *pcCos = (float*) malloc(sizeof(float) * degreeBins);
+  float *pcSin = (float*) malloc(sizeof(float) * degreeBins);
+  float rad = 0;
+
+  // Ciclo que obtiene el coseno y seno de los radiantes dados hasta llegar al límite.
+  for (int i = 0; i < degreeBins; i++) {
+    pcCos[i] = cos(rad);
+    pcSin[i] = sin(rad);
+    rad += radInc;
+  }
+
+  float rMax = sqrt(1.0 * w * w + 1.0 * h * h) / 2;
+  float rScale = ((2 * rMax) / rBins);
+
+  cudaMemcpy(d_Cos, pcCos, sizeof(float)* degreeBins, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_Sin, pcSin, sizeof(float)* degreeBins, cudaMemcpyHostToDevice);
+
+  unsigned char *d_in, *h_in;
+  int *d_hough, *h_hough;
+
+  h_in = inImg.pixels;
+
+  h_hough = (int*) malloc(degreeBins * rBins * sizeof(int));
+
+  cudaMalloc ((void **) &d_in, sizeof(unsigned char) * w * h);
+  cudaMalloc ((void **) &d_hough, sizeof(int) * degreeBins * rBins);
+  cudaMemcpy (d_in, h_in, sizeof(unsigned char) * w * h, cudaMemcpyHostToDevice);
+  cudaMemset (d_hough, 0, sizeof(int) * degreeBins * rBins);
+
+  int blockNum = ceil (w * h / 256);
+
+  cudaEvent_t start, stop;
+  float elapsedTime;
+
+  // Crear eventos CUDA
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
+  // Marcar el evento de inicio
+  cudaEventRecord(start, 0);
+
+  GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
+
+  // Marcar el evento de finalización
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+
+  // Calcular el tiempo transcurrido
+  cudaEventElapsedTime(&elapsedTime, start, stop);
+
+  // Imprimir el tiempo transcurrido
+  printf("Tiempo de ejecución del kernel: %f ms\n", elapsedTime);
+
+  cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < degreeBins * rBins; i++) {
+    if (cpuht[i] != h_hough[i]) {
+      printf ("Calculation mismatch at: %i %i %i\n", i, cpuht[i], h_hough[i]);
+    }
+  }
+
+  printf("Done!\n");
+
+  cudaFree(d_in);
+  cudaFree(d_hough);
+  cudaFree(d_Cos);
+  cudaFree(d_Sin);
+
+  free(h_hough);
+
+  delete[] cpuht;
+
+  return 0;
+}
